a5f8e913950e458c2deedddf46f7cff30b030bf3
HassIckDir?? <w2kvtygszt@privaterelay.appleid.com>
2026-08-30 17:23:43 +0300

    Update main.hpp

--- vs2026/Good RTS Engine/main.hpp
+++ vs2026/Good RTS Engine/main.hpp
@@ -1,3 +1,17 @@
+/*
+ * GoodRTS
+ * Copyright (C) Copyright (C) 2026 Batuhan Demir (Hassickdiq)
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #pragma once
 #define SCREEN_HEIGHT 720
 #define SCREEN_WIDTH 1280
